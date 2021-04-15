0b3b66fadfea8dfe8f5072d6026168b60d17bac6
pooripro <steampro35200@gmail.com>
2021-04-15 14:52:07 +0700

    [62130500075] Add tailwind to Vue and Create file Project

---
+++ src/router/index.js
@@ -0,0 +1,26 @@
+import { createRouter, createWebHistory } from 'vue-router'
+import Home from '../views/Home.vue'
+
+const routes = [
+  {
+    path: '/',
+    name: 'About',
+    component: Home
+  },
+  {
+    path: '/Pro',
+    name: 'Puwa',
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+
+  }
+]
+
+const router = createRouter({
+  history: createWebHistory(process.env.BASE_URL),
+  routes
+})
+
+export default router
