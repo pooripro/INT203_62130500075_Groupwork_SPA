6e39a5528fe66bfbcbe525392d80ec3ab4985624
pooripro <steampro35200@gmail.com>
2021-04-17 10:58:11 +0700

    [62130500075] View About Page

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,6 +1,32 @@
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
+  <div class="bg-cosmiclatte w-screen h-screen">
+    <div class="pt-12">
+        <div class="flex justify-center">
+          <img src="../assets/logo.jpg" class="rounded-full h-72 w-72 flex items-center justify-center" width="300" height="300">
+        </div>
+        <div class="pt-4">
+          <p class="font-chewy text-5xl"> {{ this.uname }}</p>
+      </div>
+    </div>
   </div>
-  
 </template>
+
+<script>
+export default {
+   mounted(){
+    console.log(this.$route);
+    console.log(`route name: `, this.$route.params.name)
+    console.log(`route id: `, this.$route.params.id)
+    this.uname = this.$route.params.name
+    console.log(`attribute uname: ${this.uname}`);
+  },
+
+  data(){
+    return{
+      uname: ''
+    }
+  },
+
+}
+
+</script>
